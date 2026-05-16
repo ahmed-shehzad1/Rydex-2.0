c85c5e01d6b6bbe708b8903a34827a7c76003969
Ahmed Shahzad <243330@students.au.edu.pk>
2026-05-16 19:47:03 +0500

    fixed avatar helper import

--- src/components/SafeRide.jsx
+++ src/components/SafeRide.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { MessageCircle, Check, X, ArrowRight, Clock, Shield, Heart, AlertCircle, Play, BookOpen } from "lucide-react"; 
 import { toast } from 'react-toastify';
 import Chat from "../components/Chat"; 
-import { getAvatarUrl } from "../utils/avatarHelper";
+import { getAvatarUrl } from "../Utils/avatarHelper";
 import "../styles/Matches.css";
 
 export default function SafeRide() {
